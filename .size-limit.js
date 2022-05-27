6e25d3b0267b50e39fa049b935231a1891673240
Pavel Bely <poul-white@yandex.ru>
2022-05-28 01:27:11 +0300

    feat: with size-limit

---
+++ .size-limit.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    path: 'dist/production/*.esm.js',
+    limit: '6.50 kB',
+  },
+  {
+    path: 'dist/production/*.umd.js',
+    limit: '6.58 kB',
+  },
+  {
+    path: 'dist/production/*maps.js',
+    limit: '6.51 kB',
+  },
+];
