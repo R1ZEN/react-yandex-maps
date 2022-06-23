c6e8319563c828c8769705e996b6f5bab8d061c6
Pavel Bely <poul-white@yandex.ru>
2022-06-23 11:29:04 +0300

    feat: remote object manager

--- examples/remote-object-manager/src/pages/index.tsx
+++ examples/remote-object-manager/src/pages/index.tsx
@@ -1,29 +1,46 @@
 import type { NextPage } from 'next';
 import { useEffect, useRef } from 'react';
-import { YMaps, useYMaps } from '../react-yandex-maps';
+import {
+  YMaps,
+  useYMaps,
+  Map,
+  RemoteObjectManager,
+} from '../react-yandex-maps';
 
 const MapWithRemoteObjectManager = () => {
-  const mapRef = useRef(null);
-  const ymaps = useYMaps(['Map', 'RemoteObjectManager']);
+  // const mapRef = useRef(null);
+  // const ymaps = useYMaps(['Map', 'RemoteObjectManager']);
+  //
+  // useEffect(() => {
+  //   if (!ymaps || !mapRef.current) {
+  //     return;
+  //   }
+  //
+  //   const map = new ymaps.Map(mapRef.current, {
+  //     center: [55.76, 37.64],
+  //     zoom: 10,
+  //   });
+  //
+  //   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //   // @ts-ignore
+  //   const rom = new ymaps.RemoteObjectManager('/api/tile?bbox=%b');
+  //
+  //   map.geoObjects.add(rom);
+  // }, [ymaps]);
+  const ref = useRef();
+  console.log(ref);
 
-  useEffect(() => {
-    if (!ymaps || !mapRef.current) {
-      return;
-    }
+  return (
+    <Map
+      defaultState={{ center: [55.76, 37.64], zoom: 10 }}
+      width="100vw"
+      height="100vh"
+    >
+      <RemoteObjectManager urlTemplate="/api/tile?bbox=%b" instanceRef={ref} />
+    </Map>
+  );
 
-    const map = new ymaps.Map(mapRef.current, {
-      center: [55.76, 37.64],
-      zoom: 10,
-    });
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const rom = new ymaps.RemoteObjectManager('/api/tile?bbox=%b');
-
-    map.geoObjects.add(rom);
-  }, [ymaps]);
-
-  return <div ref={mapRef} style={{ width: '100vw', height: '100vh' }} />;
+  // return <div ref={mapRef} style={{ width: '100vw', height: '100vh' }} />;
 };
 
 const RemoveObjectManagerExample: NextPage = () => {
