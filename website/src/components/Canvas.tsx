fae9d357106b773ae28c23701f8492fb5ebd8b7d
Pavel Bely <poul-white@yandex.ru>
2022-11-05 23:51:56 +0300

    feat: partial russian support

--- website/src/components/Canvas.tsx
+++ website/src/components/Canvas.tsx
@@ -1,27 +1,56 @@
 import React from 'react';
 import CodeBlock from '@theme/CodeBlock';
-
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+import { translate } from '@docusaurus/Translate';
+
 import * as ReactYandexMaps from '@pbe/react-yandex-maps';
 import { LiveProvider, LivePreview } from 'react-live';
+import prettier from 'prettier/standalone';
+import prettierParserBabel from 'prettier/parser-babel';
 
 interface PlaygroundCodeProps {
   children: string;
   scope?: Record<string, any>;
 }
 
-export const Canvas: React.FC<PlaygroundCodeProps> = ({ children, scope = {} }) => (
-  <LiveProvider code={children} scope={{ ...ReactYandexMaps, ...scope }}>
-    <Tabs>
-      <TabItem value="playground" label="Preview" default>
-        <CodeBlock>
-          <LivePreview />
-        </CodeBlock>
-      </TabItem>
-      <TabItem value="code" label="Source Code">
-        <CodeBlock language="jsx">{children}</CodeBlock>
-      </TabItem>
-    </Tabs>
-  </LiveProvider>
+const formatCode = (code: string) =>
+  prettier.format(code, {
+    plugins: [prettierParserBabel],
+  });
+
+export const Canvas: React.FC<PlaygroundCodeProps> = ({
+  children,
+  scope = {},
+}) => (
+  <BrowserOnly>
+    {() => (
+      <LiveProvider code={children} scope={{ ...ReactYandexMaps, ...scope }}>
+        <Tabs>
+          <TabItem
+            value="preview"
+            label={translate({
+              message: 'Просмотр',
+              id: 'components.canvas.preview',
+            })}
+            default
+          >
+            <CodeBlock>
+              <LivePreview />
+            </CodeBlock>
+          </TabItem>
+          <TabItem
+            value="code"
+            label={translate({
+              message: 'Исходный код',
+              id: 'components.canvas.code',
+            })}
+          >
+            <CodeBlock language="jsx">{formatCode(children)}</CodeBlock>
+          </TabItem>
+        </Tabs>
+      </LiveProvider>
+    )}
+  </BrowserOnly>
 );
