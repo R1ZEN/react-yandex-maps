02ad72a60537805a2e157d4ad5865d5d7850cf09
Arman <mail@arman.io>
2022-11-10 23:01:37 +0100

    fix `RoutePanelProps` types (#34)
    bump `@types/yandex-maps`

--- src/controls/RoutePanel.tsx
+++ src/controls/RoutePanel.tsx
@@ -15,20 +15,20 @@ interface RoutePanelProps extends Omit<BaseControlProps, 'name'> {
   /**
    * Control [options](https://tech.yandex.com/maps/doc/jsapi/2.1/ref/reference/control.RoutePanel-docpage/#control.RoutePanel__param-parameters.options)
    */
-  options?: control.IRouteButtonParameters['options'];
+  options?: control.IRoutePanelParameters['options'];
   /**
    * Uncontrolled control [options](https://tech.yandex.com/maps/doc/jsapi/2.1/ref/reference/control.RoutePanel-docpage/#control.RoutePanel__param-parameters.options)
    */
-  defaultOptions?: control.IRouteButtonParameters['options'];
+  defaultOptions?: control.IRoutePanelParameters['options'];
 
   /**
    * Control [state](https://tech.yandex.com/maps/doc/jsapi/2.1/ref/reference/control.RoutePanel-docpage/#control.RoutePanel__param-parameters.state)
    */
-  state?: control.IRouteButtonParameters['state'];
+  state?: control.IRoutePanelParameters['state'];
   /**
    * Uncontrolled control [state](https://tech.yandex.com/maps/doc/jsapi/2.1/ref/reference/control.RoutePanel-docpage/#control.RoutePanel__param-parameters.state)
    */
-  defaultState?: control.IRouteButtonParameters['state'];
+  defaultState?: control.IRoutePanelParameters['state'];
 }
 
 export const RoutePanel: React.FC<
